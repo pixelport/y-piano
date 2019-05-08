e6ee225ad24653bee3a6bb48be5082f1d59ef3c8
pixelport <johanngillhoff@yahoo.de>
2019-05-08 17:56:29 +0200

    click on piano key plays tone, works also while keeping the mouse pressed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Keyboard} from "./Keyboard";
+import Tone from 'tone';
 
 class App extends Component {
   
   constructor (){
     super();
+
+    //create a synth and connect it to the master output (your speakers)
+    this.synth = new Tone.Synth().toMaster()
   }
   
-  playTone = () => {
-    
+  playNote = (note) => {
+    //play the note for the duration of an 8th note
+    this.synth.triggerAttackRelease(note, '8n')
   }
   
   render() {
@@ -18,7 +23,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <p>Test</p>
-          <Keyboard/>
+          <Keyboard playNote={this.playNote}/>
         </header>
       </div>
     );
--- src/Keyboard.js
+++ src/Keyboard.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import './Keyboard.css';
 
-export const Keyboard = () => {
+const whiteKeys = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+
+const blackKeys = [
+  "C#", "Eb", null, "F#", "G#", "Bb"
+];
+
+export const Keyboard = ({playNote}) => {
+
+  const onMouseOver = (e, note) => {
+    if(e.nativeEvent.buttons === 1)
+      playNote(note)
+  };
   
-  const keys = []
+  const keys = [];
   for(let i = 0; i < 14; i++){
-    keys.push(<div key={i} className="piano-key"></div>)
     const key = i % 7;
+    const octave = 4 + Math.floor(i / 7);
+    
+    // white key
+    const onWhiteKey = (e) => onMouseOver(e, whiteKeys[key] + octave);
+    keys.push(<div key={i} className="piano-key" onMouseDown={onWhiteKey} onMouseOver={onWhiteKey} />);
+    
+    // black key
     if(key !== 2 && key !== 6) {
-      keys.push(<div key={i + 'b'} className="piano-key key-black"></div>)
+      const onBlackKey = (e) => onMouseOver(e, blackKeys[key] + octave);
+      keys.push(<div key={i + 'b'} className="piano-key key-black" onMouseDown={onBlackKey} onMouseOver={onBlackKey} />)
     }
   }
   
